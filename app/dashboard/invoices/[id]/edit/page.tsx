8a80ed590812ba3c6a7fd4029dc604e270d101ab
Daniel Riley <dann.riley@icloud.com>
2024-01-08 19:11:08 +0100

    fixed some bugs regarding login auth

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,7 @@
-import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
-import { updateInvoice } from "@/app/lib/actions";
-import { notFound } from "next/navigation";
+import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import Form from "@/app/ui/invoices/create-form";
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -14,6 +13,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   if (!invoice) {
     notFound();
   }
+
   return (
     <main>
       <Breadcrumbs
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,20 +12,13 @@ import { formatCurrency } from "./utils";
 import { unstable_noStore as noStore } from "next/cache";
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore();
-
   try {
-    // Artificially delay a reponse for demo purposes.
-    // Don't do this in real life :)
-
     console.log("Fetching revenue data...");
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log("Data fetch complete after 3 seconds.");
+    console.log("Data fetch completed after 3 seconds.");
 
     return data.rows;
   } catch (error) {
@@ -173,6 +166,7 @@ export async function fetchInvoiceById(id: string) {
     return invoice[0];
   } catch (error) {
     console.error("Database Error:", error);
+    throw new Error("Failed to fetch invoice.");
   }
 }
 
